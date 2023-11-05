eba971a7707fa1a6b17700e0dcd536d639a5e6e8
Ronald Gutierrez <ronaldgutierrezj@gmail.com>
2023-11-05 14:50:18 -0600

    evets app one to one relationship done

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -2,14 +2,12 @@ package org.launchcode.codingevents.models;
 
 import jakarta.persistence.*;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-
-
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Chris Bay
@@ -32,6 +30,9 @@ public class Event extends AbstractEntity{
     @ManyToOne
     @NotNull(message = "Category is required")
     private EventCategory eventCategory;
+
+    @ManyToMany
+    private final List<Tag> tags = new ArrayList<>();
 
     public Event(String name, EventCategory eventCategory) {
         this.name = name;
@@ -61,6 +62,12 @@ public class Event extends AbstractEntity{
 
     public void setEventCategory(EventCategory eventCategory) { this.eventCategory = eventCategory; }
 
+    public List<Tag> getTags() {
+        return tags;
+    }
+    public void addTag(Tag tag){
+        this.tags.add(tag);
+    }
 
     @Override
     public String toString() {
--- src/main/java/org/launchcode/codingevents/models/Tag.java
+++ src/main/java/org/launchcode/codingevents/models/Tag.java
@@ -2,7 +2,11 @@ package org.launchcode.codingevents.models;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Entity
@@ -10,6 +14,9 @@ public class Tag extends AbstractEntity {
 
     @Size(min=1, max = 25, message="Name must be at least 1 characters long")
     private String name;
+
+    @ManyToMany(mappedBy = "tags")
+    private final List<Event> events = new ArrayList<>();
 
     public Tag(String name) {
         this.name = name;
@@ -26,6 +33,10 @@ public class Tag extends AbstractEntity {
     }
 
     public String getDisplayName(){ return "#" + name + " ";}
+
+    public List<Event> getEvents() {
+        return events;
+    }
 
     @Override
     public String toString(){
